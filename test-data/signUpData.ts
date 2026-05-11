2e0e6fcbb8dc6effc85eb1f1da7e9ded96ecc892
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-05-11 12:02:44 +0200

    Updated browsers to use only chromium.

--- test-data/signUpData.ts
+++ test-data/signUpData.ts
@@ -2,7 +2,7 @@ export const signUpData = {
     validSignUp: {
         firstName:'Zoleka',
         lastName:'Saki',
-        email:'Zolekasaki@gmail.com',
+        email:'Zolekaasaki@gmail.com',
         password: 'England@123456',
         confirmPassword:'England@123456',
         group:'2nd Group 2026 (2026)'
