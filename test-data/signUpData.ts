4df5da3429f1518ce9e5789edb1824c4e0df9a64
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-07-23 15:14:13 +0200

    Updated test data.

--- test-data/signUpData.ts
+++ test-data/signUpData.ts
@@ -1,8 +1,8 @@
 export const signUpData = {
     validSignUp: {
-        firstName:'Zoleka',
-        lastName:'Saki',
-        email:'Zolekaasaki@gmail.com',
+        firstName:'Manqonqo',
+        lastName:'Developer',
+        email:'Developer@gmail.com',
         password: 'England@123456',
         confirmPassword:'England@123456',
         group:'2nd Group 2026 (2026)'
